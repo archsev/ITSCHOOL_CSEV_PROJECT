bb1085866549dee1ec31ddb8a8a5f9e2a03cd2e3
archsev <archsev@gmail.com>
2022-11-23 18:25:14 +0200

    update

--- project _setup.sh
+++ project _setup.sh
@@ -9,4 +9,11 @@
 3. Ansible 
     configure ansible to allow SSH or add "--conection=local"
     install "docker" using ansible playbook "ansible-playbook -i inv.yml ansible_docker.yml --connection=local"
+    install docker-ce
+    Manage Docker as a non-root user https://docs.docker.com/engine/install/linux-postinstall/#manage-docker-as-a-non-root-user
     install "jenkins" using ansible playbook "ansible-playbook -i inv.yml ansible_jenkins.yml --connection=local"
+4.jenkins
+    configure jenkins
+    install plugins: Convert to Pipeline, Credentials, Git, etc.
+    create build
+    configure GIT Credentials
